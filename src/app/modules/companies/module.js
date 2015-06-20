8475b89febc400417f8722775594c9e8feeb3fce
maticaputti <matias.caputti@gmail.com>
2015-06-20 10:29:11 -0300

    Start works module

--- src/app/modules/companies/module.js
+++ src/app/modules/companies/module.js
@@ -1,43 +1,42 @@
-/**
- * Created by emiliano on 12/06/15.
- */
+
+'use strict';
 
 angular.module('companiesModule', ['firebase', 'fireQuery'])
-  .config(function($stateProvider, $urlRouterProvider) {
-    //No unir con el auth module, ya que esto sera particular para esta solucion.
-    $stateProvider
-      .state('app.registerCompany', {
-        url: '/company/register',
-        controller : 'companiesCtrl as register',
-        templateUrl: 'app/modules/companies/views/registerCompany.html',
-        resolve: {
-          param1 : function() {
-            return null
-          }
-        }
-      })
-      .state('app.companiesList', {
-        url: '/company/list',
-        controller : 'companiesCtrl as list',
-        templateUrl: 'app/modules/companies/views/companiesList.html',
-        resolve: {
-          param1 : function(companiesService) {
-            return companiesService.showCompanies()
-          }
-        }
-      })
-      .state('app.addCompanyInformation', {
-        url: '/company/addinfo/:companyId',
-        controller : 'companiesCtrl as information',
-        templateUrl: 'app/modules/companies/views/addCompanyInformation.html',
-        resolve: {
-          param1 : function(companiesService, $stateParams) {
-            var companyId = $stateParams.companyId;
-            return companiesService.getCompanyById(companyId)
-          }
-        }
-      })
 
-  })
+    .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('app.registerCompany', {
+                url: '/company/register',
+                controller : 'companiesCtrl as register',
+                templateUrl: 'app/modules/companies/views/registerCompany.html',
+                resolve: {
+                    param1 : function() {
+                        return null
+                    }
+                }
+            })
+            .state('app.listCompanies', {
+                url: '/company/list',
+                controller : 'companiesCtrl as list',
+                templateUrl: 'app/modules/companies/views/listCompanies.html',
+                resolve: {
+                    param1 : function(companiesService) {
+                        return companiesService.showCompanies()
+                    }
+                }
+            })
+            .state('app.updateCompany', {
+                url: '/company/update/:companyId',
+                controller : 'companiesCtrl as update',
+                templateUrl: 'app/modules/companies/views/updateCompany.html',
+                resolve: {
+                    param1 : function(companiesService, $stateParams) {
+                        var companyId = $stateParams.companyId;
+                        return companiesService.getCompanyById(companyId)
+                    }
+                }
+            })
+
+    })
 
 
--- src/app/modules/works/module.js
+++ src/app/modules/works/module.js
@@ -3,12 +3,39 @@
 
 angular.module('worksModule', [])
 
-	.config(function ($stateProvider, $urlRouterProvider) {
+	.config(function($stateProvider, $urlRouterProvider) {
 		$stateProvider
-			.state('app.works', {
-				url: '/works',
-				controller : 'worksCtrl as works',
-        		templateUrl: 'app/modules/works/views/works.html',
+			.state('app.registerWork', {
+				url: '/work/register/:companyId',
+				controller : 'worksCtrl as register',
+				templateUrl: 'app/modules/companies/views/registerWork.html',
+				resolve: {
+					param1 : function() {
+						return null
+					}
+				}
+			})
+			.state('app.listWorks', {
+				url: '/work/list/:companyId',
+				controller : 'worksCtrl as list',
+	        	templateUrl: 'app/modules/works/views/listWorks.html',
+				resolve: {
+					param1 : function(companiesService, $stateParams) {
+						var companyId = $stateParams.companyId;
+						return companiesService.getCompanyById(companyId)
+					}
+				}
+			})
+			.state('app.updateWork', {
+				url: '/work/update/:workId',
+				controller : 'worksCtrl as update',
+				templateUrl: 'app/modules/companies/views/updateWork.html',
+				resolve: {
+					param1 : function(companiesService, $stateParams) {
+						var companyId = $stateParams.companyId;
+						return companiesService.getCompanyById(companyId)
+					}
+				}
 			})
 	})
 	
