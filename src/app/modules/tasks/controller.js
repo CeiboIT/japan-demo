9f63a20682f292c38f7368970bd5fc2268f8dffd
maticaputti <matias.caputti@gmail.com>
2015-06-25 12:16:41 -0300

    Added 'tasks' module

--- src/app/modules/tasks/controller.js
+++ src/app/modules/tasks/controller.js
@@ -16,3 +16,79 @@ angular
 	.module('tasksModule')
 	.controller('tasksCtrl', tasksCtrl)
 
+
+
+
+'use strict';
+
+var worksCtrl = function(worksService, companiesService, param1, param2) {
+console.log("works controller");
+	var work = this;
+
+    if (param1 !== null) {
+        work.works = param1; // all work ids
+    }
+
+    if (param2 !== null) {
+        work.allworks = param2; // all full works
+
+        // wire-style populate
+        work.workspop = [];
+
+        for (var id in work.works.works) { // para cada id
+            for (var fullwork in work.allworks) {
+                if (work.works.works[id] == work.allworks[fullwork].$id) {
+                    work.workspop.push(work.allworks[fullwork]);
+                }
+            }
+        }
+    }
+
+
+
+    work.options = {
+        types: '(cities)',
+        watchEnter: true,
+        //country: 'ar'
+    };
+
+    work.details;
+
+    work.createWork = function(work) {
+        work.createPromise = worksService.createWork(work)
+            .then(function(data) {
+				//console.log("work: ", data);
+				if (param1.works) {
+					param1.works.push(data.id);
+				} else {
+					param1.works = [];
+					param1.works.push(data.id);
+				}
+				companiesService.updateCompany(param1);
+            }, function(error) {
+                work.error = error;
+                //console.log("error creating work", work.error);
+            });
+    };
+
+    work.updateWork = function(dataToUpdate) {
+        work.updateWork = worksService.updateWork(dataToUpdate)
+            .then (function() {
+                console.log("update ok");
+            }, function(error) {
+                work.error = error;
+                console.log("update no ok", work.error);
+            });
+    };
+
+	function init() {
+    	
+	};
+
+	//INITIALIZING
+	init()
+
+};
+
+angular.module('worksModule')
+  .controller('worksCtrl', worksCtrl);
--- src/app/modules/tasks/services.js
+++ src/app/modules/tasks/services.js
@@ -1,10 +1,76 @@
 
 'use strict';
 
-var tasksService = function($http, apiBaseUrl) {
-	
-};
+
+var worksService = function(fireRef, Kutral, $q, $firebaseArray) {
+
+  var service = this;
+
+  var kutral  = new Kutral(fireRef);
+
+  var workSchema = new Kutral.Schema({
+      'title': {type: String, indexOn: true},
+      'location': {type: String},
+      'members': [{type: 'ObjectId', ref:'users'}],
+      'tasks': [{type: 'ObjectId', ref:'tasks'}],
+  });
+
+  var worksDirectory = kutral.model('works', workSchema);
+
+  service.showWorks = function(){
+    var worksPromise = $q.defer();
+
+    worksDirectory.find().$asArray(function(data) {
+      worksPromise.resolve(data);
+    });
+
+    return worksPromise.promise;
+  }
+
+  service.getWorkById = function(id){
+    var getWorkByIdPromise = $q.defer();
  
-angular
-	.module('tasksModule')
-	.service('tasksService', tasksService)
+    worksDirectory.find({$id:id}, function(data) {
+      getWorkByIdPromise.resolve(data);
+    });
+ 
+    return getWorkByIdPromise.promise;
+  }
+
+  service.createWork = function(work) {
+    var workCreationPromise = $q.defer();
+    worksDirectory.data = work;
+    worksDirectory.create(function(createdWork) {
+      workCreationPromise.resolve(createdWork);
+    });
+
+    return workCreationPromise.promise;
+  };
+
+  service.updateWork = function(dataToUpdate) {
+    var updateArticlePromise = $q.defer();
+    worksDirectory.data = dataToUpdate;
+
+    worksDirectory.update(function(success) {
+      updateArticlePromise.resolve(worksDirectory.data);
+    });
+
+    return updateArticlePromise.promise;
+
+  };
+
+  service.checkWorkTitleAvailability = function(name) {
+      var titleAvailabilityPromise = $q.defer();
+      worksDirectory.find({"name" : name}, function(workData) {
+        titleAvailabilityPromise.resolve(!workData)
+      });
+
+      return titleAvailabilityPromise.promise;
+  };
+
+
+};
+
+
+angular.module('worksModule')
+  .service('worksService', worksService)
